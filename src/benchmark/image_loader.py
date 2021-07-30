4ed128ca2d495fb821eb8172da83db4dfe8663c0
Ivan Svogor <ivan.svogor@iarai.ac.at>
2021-07-30 17:26:50 +0200

    WIP: Pooled ActionPlayer added

--- src/benchmark/image_loader.py
+++ src/benchmark/image_loader.py
@@ -30,8 +30,9 @@ def load_random_to_gpu():
     torch.rand(469, 387).cuda()
 
 
-def test_tensor_loading(create_tensor_fn, warmup_cycle=False, repeat=10):
-    action_player = ActionPlayer()
+def test_tensor_loading(create_tensor_fn, warmup_cycle=False, repeat=10, action_player=None):
+    if action_player is None:
+        action_player = ActionPlayer()
     # warmup cycle
     action_name = create_tensor_fn.__name__
     if warmup_cycle:
--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
+#! /usr/bin/python3
 import argparse
 
 from benchmark.data_loader import test_s3, test_scratch
 from benchmark.image_loader import test_tensor_loading, load_image, load_random_on_gpu, load_random_to_gpu
+from misc.mp_action_player import MPActionPlayer
 
 
 def handle_arguments():
@@ -15,26 +17,33 @@ def handle_arguments():
     return parser
 
 
+def hello(item: int):
+    print(f"Hello.... {item}")
+
 if __name__ == "__main__":
     # interpret arguments
     parser = handle_arguments()
     args = parser.parse_args()
 
     # load dataset
-    dataset = args.dataset
+    # dataset = args.dataset
 
-    if args.benchmark == "s3":
-        test_s3(dataset)
-    elif args.benchmark == "scratch":
-        test_scratch(dataset)
-    elif args.benchmark == "random_gpu":
-        test_tensor_loading(load_random_on_gpu, False, 200)
-        test_tensor_loading(load_random_on_gpu, True, 200)
-    elif args.benchmark == "random_to_gpu":
-        test_tensor_loading(load_random_to_gpu, False, 200)
-        test_tensor_loading(load_random_to_gpu, True, 200)
-    elif args.benchmark == "random_image":
-        test_tensor_loading(load_image, False, 200)
-        test_tensor_loading(load_image, True, 200)
-    else:
-        parser.print_help()
+    # if args.benchmark == "s3":
+    #     test_s3(dataset)
+    # elif args.benchmark == "scratch":
+    #     test_scratch(dataset)
+    # elif args.benchmark == "random_gpu":
+    #     test_tensor_loading(load_random_on_gpu, False, 200)
+    #     test_tensor_loading(load_random_on_gpu, True, 200)
+    # elif args.benchmark == "random_to_gpu":
+    #     test_tensor_loading(load_random_to_gpu, False, 200)
+    #     test_tensor_loading(load_random_to_gpu, True, 200)
+    # elif args.benchmark == "random_image":
+    #     test_tensor_loading(load_image, False, 200)
+    #     test_tensor_loading(load_image, True, 200)
+    # else:
+    #     parser.print_help()
+    
+    test_tensor_loading(load_image, True, 200)
+    action_player = MPActionPlayer(num_workers=8, pool_size=4)
+    test_tensor_loading(load_image, True, 200, action_player)
