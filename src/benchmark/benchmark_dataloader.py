42c4bfc6a5829aad882b48638b9a1db8e7fce452
Ivan Svogor <ivan.svogor@iarai.ac.at>
2021-08-12 16:13:01 +0200

    added as clas

--- src/benchmark/benchmark_dataloader.py
+++ src/benchmark/benchmark_dataloader.py
@@ -7,75 +7,67 @@ from torch.utils.data import DataLoader
 IMAGENET_PATH_SCRATCH = "/scratch/imagenet"
 
 
-dataset = ScratchDataset(IMAGENET_PATH_SCRATCH, "val")
-        # dataset.index_all()
-        # dataset.save_index()
-dataset.load_index()
-batch_size = 10
-num_workers = 4
+class BechmarkDataloader:
+    def __init__(self, batch_size: int = 1, num_workers: int = 1, dataset: str = "val") -> None:
+        self.batch_size = batch_size
+        self.num_workers = num_workers
 
+        # todo extend to S3
+        self.dataset = ScratchDataset(IMAGENET_PATH_SCRATCH, "val")
+        self.dataset.load_index()
 
-def set_dataloader(self, dataloader: str = "async"):
-    if dataloader == "async":
-        use_async_dataloader()
-    else:
-        use_torch_dataloader()
+        # note: dataloader cannot be the part of this class due to pickling error
+        # todo https://discuss.pytorch.org/t/torch-cuda-streams-stream-in-multiprocess-context-causes-error-cant-pickle-stream-objects/80625
 
-def use_torch_dataloader():
-    return DataLoader(
-        dataset=dataset, 
-        batch_size=batch_size, 
-        num_workers=num_workers, 
-        shuffle=False, 
-        collate_fn=collate
-    )
+    def create_dataloader(self, dataloader: str = "torch"):
+        if dataloader == "torch":
+            return DataLoader(
+                dataset=self.dataset,
+                batch_size=self.batch_size,
+                num_workers=self.num_workers,
+                shuffle=False,
+                collate_fn=self.collate,
+            )
+        else:
+            return AsynchronousLoader(
+                data=self.dataset,
+                batch_size=self.batch_size,
+                num_workers=self.num_workers,
+                shuffle=False,
+                device=torch.device("cuda"),
+                collate_fn=self.collate,
+            )
 
-def use_async_dataloader():
-    return AsynchronousLoader(
-        data=dataset,
-        batch_size=batch_size,
-        num_workers=num_workers,
-        shuffle=False,
-        device=torch.device("cuda"),
-        collate_fn=collate,
-    )
+    def collate(self, batch):
+        imgs = [item for item in batch]
+        return imgs
 
-def collate(batch):
-    imgs = [item for item in batch]
-    return imgs
+    def load_single(self, dataloader):
+        try:
+            n = next(iter(dataloader))
+            print(len(n), len(n[0]))
+        except StopIteration:
+            pass
 
-default_dataloader = use_async_dataloader()
+    def load_all(self, dataloader):
+        # get single
+        try:
+            for i, batch in enumerate(dataloader):
+                if i % 1000 == 0:
+                    print(f"{len(batch)}, {i}")
+            # print(len(n), len(n[0]))
+        except (StopIteration, EOFError) as e:
+            print(f"Exception raised: {str(e)}")
 
-
-def load_single():
-    try:
-        n = next(iter(default_dataloader))
-        print(len(n), len(n[0]))
-    except StopIteration:
-        pass
-
-def load_all():
-    # get single
-    try:
-        for i, batch in enumerate(default_dataloader):
-            if i % 1000 == 0:
-                print(f"{len(batch)}, {i}")
-        # print(len(n), len(n[0]))
-    except (StopIteration, EOFError) as e:
-        print(f"Exception raised: {str(e)}")
 
 def benchmark_scratch_dataloader():
     action_player = ActionPlayer()
-    #bm = BechmarkDataloader(10, 4)
+    bm = BechmarkDataloader(4, 2)
+    dl = bm.create_dataloader("async")
 
     # todo calculate runtime for batches, and images per batch, try out different sizes of batchers and workers...
-    # todo https://discuss.pytorch.org/t/torch-cuda-streams-stream-in-multiprocess-context-causes-error-cant-pickle-stream-objects/80625
-    # todo, for some reason, this donesn't work as a class (the reason above)
-    
-    # action_player.benchmark("loading_with_dataloader", bm.load_single, 2)
-    # action_player.benchmark("loading_with_dataloader", bm.load_all, 2)
 
     print("Dataloader benchmark")
-    # action_player.benchmark("loading_with_dataloader", load_single, 2)
-    action_player.benchmark("loading_with_dataloader", load_all, 2)
+    # action_player.benchmark("loading_with_dataloader", lambda: bm.load_single(dl), 2)
+    action_player.benchmark("loading_with_dataloader", lambda: bm.load_all(dl), 2)
     print("Done...")
--- src/misc/random_generator.py
+++ src/misc/random_generator.py
@@ -3,10 +3,11 @@ from typing import Optional
 
 SEED = 10
 
+
 class RandomGenerator:
     def __init__(self, seed: Optional[int] = SEED):
         self.rng = Random()
         self.rng.seed(SEED)
-        
+
     def get_int(self, a, b):
         return self.rng.randint(a, b)
