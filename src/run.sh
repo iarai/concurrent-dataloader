8acbce02e64f735a17e28483ecb8b950741b54d7
Christian Eichenberger <christian.eichenberger@iarai.ac.at>
2021-09-13 12:42:56 +0200

    run.sh with more combinations num_works vs. fetch_workers

--- src/run.sh
+++ src/run.sh
@@ -5,7 +5,7 @@
 for storage in "s3" "scratch"; do
   for num_fetch_workers in 1 2 4 8 16 32; do
     for num_workers in 1 2 4 8 16 32 64 128; do
-      python -m benchmark.benchmark_dataloader --output_base_folder ~/workspaces/storage-benchmarking/benchmark_output --dataset scratch  --num_fetch_workers --num_fetch_workers ${num_fetch_workers} --num_workers  ${num_workers} --repeat 1 --num_batches 40 --batch_size 32 --prefetch_factor 2
+      python -m benchmark.benchmark_dataloader --output_base_folder ~/workspaces/storage-benchmarking/benchmark_output --dataset "${storage}"  --num_fetch_workers "${num_fetch_workers}" --num_workers  "${num_workers}" --repeat 1 --num_batches 40 --batch_size 32 --prefetch_factor 2
     done
   done
 done
@@ -15,7 +15,7 @@ done
 # num_workers=0, varying num_fetch_workers
 for storage in "s3" "scratch"; do
   for num_fetch_workers in 1 2 4 8 16 32 64 128; do
-    python -m benchmark.benchmark_dataloader --output_base_folder ~/workspaces/storage-benchmarking/benchmark_output --dataset ${storage}  --num_workers 0 --num_fetch_workers ${num_fetch_workers} --repeat 1 --num_batches 40 --batch_size 128 --prefetch_factor 2
+    python -m benchmark.benchmark_dataloader --output_base_folder ~/workspaces/storage-benchmarking/benchmark_output --dataset "${storage}"  --num_workers 0 --num_fetch_workers "${num_fetch_workers}" --repeat 1 --num_batches 40 --batch_size 128 --prefetch_factor 2
   done
 done
 
@@ -25,7 +25,7 @@ done
 # dataset only
 for storage in "s3" "scratch"; do
   for pool_size in 1 2 3 4 5 6 7 10 15 20 30 40 50 60 80; do
-    python -m benchmark.benchmark_dataset --output_base_folder ~/workspaces/storage-benchmarking/benchmark_output --dataset ${storage} --num_get_random_item 2000 --pool_size 0
+    python -m benchmark.benchmark_dataset --output_base_folder ~/workspaces/storage-benchmarking/benchmark_output --dataset "${storage}" --num_get_random_item 2000 --pool_size "${pool_size}"
   done
 done
 
