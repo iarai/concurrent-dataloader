54f5f507897bfbd205828f0ada1c1ddfe1b14a79
Ivan Svogor <ivan.svogor@lnx-fat-1.lan.iarai.ac.at>
2022-01-04 13:10:50 +0100

    merged with head

--- src/benchmarking/experiment_src/e2e_imagenet_torch.py
+++ src/benchmarking/experiment_src/e2e_imagenet_torch.py
@@ -44,7 +44,7 @@ parser.add_argument(
 parser.add_argument(
     "-j", "--num-workers", default=4, type=int, metavar="N", help="number of data loading workers (default: 4)"
 )
-parser.add_argument("--epochs", default=80, type=int, metavar="N", help="number of total epochs to run")  # default 90
+parser.add_argument("--epochs", default=100, type=int, metavar="N", help="number of total epochs to run")  # default 90
 parser.add_argument("--start-epoch", default=0, type=int, metavar="N", help="manual epoch number (useful on restarts)")
 parser.add_argument(
     "--lr", "--learning-rate", default=0.1, type=float, metavar="LR", help="initial learning rate", dest="lr"
