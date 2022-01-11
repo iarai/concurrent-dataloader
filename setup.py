2ff27ca7015f856b07b6d2f22d18efb9cdb6773b
Ivan Svogor <ivan.svogor@iarai.ac.at>
2022-01-11 17:02:54 +0100

    pre-commit and name changed to concurrent_dataloader

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 import setuptools
 
 setuptools.setup(
-    name="fast-torch-dataloader",
-    version="0.0.1",
+    name="concurrent-dataloader",
+    version="0.0.2",
     author="IARAI - Institute of Advanced Research in Artificial Intelligence",
     author_email="ivan.svogor@iarai.ac.at, christian.eichenberger@iarai.ac.at, moritz.neun@iarai.ac.at",
-    description="Dataloader improvements for faster, parallel data loading",
+    description="Dataloader with concurrency improvements",
     url="https://gitlab.lan.iarai.ac.at/division-t/administration/storage-benchmarking",
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
