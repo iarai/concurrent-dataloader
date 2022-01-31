49d48449797d96b8a1a5590e14e3acf3f5baf59f
Ivan Svogor <ivan.svogor@iarai.ac.at>
2022-01-31 15:36:36 +0100

    Updated lightning - no-reset test

---
+++ src/benchmarking/experiments/updated_lightning/updated_lightning_no_reset.sh
@@ -0,0 +1,20 @@
+# for fetch_impl in "threaded" "asyncio" "vanilla" ; do
+for fetch_impl in "vanilla" ; do
+    for storage in "s3" "scratch"; do
+        # for implementation in "../../experiment_src/e2e_imagenet_lightning.py" "../../experiment_src/e2e_imagenet_torch.py" ; do
+        for implementation in "../../experiment_src/e2e_imagenet_lightning.py" "../../experiment_src/e2e_imagenet_torch.py" ; do
+            python3 "${implementation}" --output_base_folder "../../../../benchmark_output/updated_lightning_no_reset_2" \
+            --dataset "${storage}" \
+            --num-fetch-workers 16 \
+            --num-workers 4 \
+            --batch-pool 512 \
+            --dataset-limit 15000 \
+            --batch-size 256 \
+            --epochs 15 \
+            --prefetch-factor 2 \
+            --fetch-impl "${fetch_impl}" \
+            --use-cache 1 \
+            --num_sanity_val_steps 0
+        done
+    done
+done
